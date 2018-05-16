d976294679470d24e053403597645d86f4d9085d
PrestonGB <preston.bebas1@gmail.com>
2018-05-16 13:49:27 -0400

    Added some javascript

---
+++ script.js
@@ -0,0 +1,98 @@
+var database = [
+  {
+    firstName: "Preston",
+    lastName: "Bebas",
+    birthMonth: "November",
+    location: "New York",
+    age: 29,
+    funFact: "You Love Spider-Man!",
+  },
+  {
+    firstName: "Petra",
+    lastName: "Bebas",
+    birthMonth: "March",
+    location: "New York",
+    age: 34,
+    funFact: "You Love Knitting!",
+  },
+  {
+    firstName: "Marilee",
+    lastName: "Pray",
+    birthMonth: "July",
+    location: "New York",
+    age: "I didn't know Emilee had a younger sister!",
+    funFact: "You are a mermaid!",
+  },
+  {
+    firstName: "Brian",
+    lastName: "Stern",
+    birthMonth: "January",
+    location: "New York",
+    age: 39,
+    funFact: "You Love Tennis!",
+  },
+
+];
+
+
+var firstNamePrompt = prompt("What is your first name (please capitalize first letter)?");
+var lastNamePrompt = prompt("What is your last name (please capitalize first letter)?");
+
+function isUserValid(first, last) {
+  for (var i = 0; i < database.length; i++) {
+    if (first === database[i].firstName && last === database[i].lastName) {
+      alert("Welcome " + first + ", let me guess some things about you!");
+      alert("You were born in the month of " + database[i].birthMonth);
+      alert("You live in " + database[i].location);
+      alert("You are this many years old... " + database[i].age);
+      alert("And.... " + database[i].funFact)
+      return true;
+    }
+  } return false;
+}
+
+function signIn(first, last) {
+  if (isUserValid(first,last) === true) {
+    return;
+  } else {
+    alert("I do not know who you are. Please try again");
+  }
+}
+
+signIn(firstNamePrompt, lastNamePrompt);
+
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick () {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterEnter (event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+
+ul.addEventListener("click", function(event) {
+	event.target.classList.toggle("done");
+});
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterEnter);
---
+++ script2.js
@@ -0,0 +1,67 @@
+var database = [
+  {
+    username: "Preston",
+    password: "preston",
+  },
+  {
+    username: "Marilee",
+    password: "salielit1",
+  },
+  {
+    username: "Petra",
+    password: "gracie",
+  }
+
+];
+
+var newsFeed = [
+  {
+    username: "bobby",
+    timeline: "so sleepy dog",
+  },
+  {
+    username: "tammy",
+    timeline: "wassssssupppp",
+  },
+  {
+    username: "donny",
+    timeline: "mesohoney",
+  }
+];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("What is your password?");
+
+function isUserValid(user, pass) {
+  for (var i = 0; i < database.length; i++) {
+    if (user === database[i].username && pass === database[i].password) {
+      return true;
+    }
+  } return false;
+}
+
+function signIn(user, pass) {
+  if (isUserValid(user, pass) === true) {
+    console.log(newsFeed);
+  } else {
+    alert("incorrect login");
+  }
+}
+
+signIn(userNamePrompt, passwordPrompt);
+
+var todos = [
+  "clean room",
+  "brush teeth",
+  "exercise",
+  "study javascript",
+  "eat healthy"
+];
+
+for (var i = 0; i < todos.length; i++) {
+  todos[i] = todos[i]+ "!";
+}
+
+todos.forEach(function(i) {
+  console.log(i);
+})
